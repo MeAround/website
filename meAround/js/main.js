ffe6ed4097f173ba5a6331f880254f333022f587
itreverie <brendadhk@gmail.com>
2017-06-10 12:28:57 +0100

    Adding the first version of the website.

---
+++ meAround/js/main.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function (){
+ $("#registration-form").hide();
+
+ $("#Continue").click(function(){
+     $("#registration-form").show();
+ });
+ $("#Register").click(function(){
+     $("#registration-form").hide();
+ });
+
+});
